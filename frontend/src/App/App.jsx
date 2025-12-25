ed78a4302d09cbd459e90b63e49658f60ae6e3f7
Tmribob <mmitko2141@gmail.com>
2025-12-25 20:24:01 +0300

    start front

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -1,10 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+import {Route, Routes} from "react-router-dom";
 
   return (
     <>
@@ -33,3 +27,7 @@ function App() {
 }
 
 export default App
+export default () => {
+  return (<Routes>
+  </Routes>);
+};
